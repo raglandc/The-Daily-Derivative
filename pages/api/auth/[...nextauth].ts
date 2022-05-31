0af065300505c1cca1fcc816985799aab328fee8
Chris Ragland <Chrisragland97@gmail.com>
2022-05-31 13:19:24 -0400

    changes before forward work

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -36,14 +36,4 @@ export default NextAuth({
     }),
   ],
   useSecureCookies: process.env.NODE_ENV === "development" ? false : true,
-  callbacks: {
-    async session({ session, token, user }) {
-      //send properties to the client, like an access_token from a provider
-      console.log("Session: ", session.accessToken);
-      console.log("\nToken: ", token);
-      console.log("\nUser: ", user);
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
 });
