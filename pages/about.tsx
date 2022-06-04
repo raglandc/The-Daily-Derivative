48ef94677f939f0663927086c26e163883e5cd03
Chris Ragland <Chrisragland97@gmail.com>
2022-06-04 14:57:29 -0400

    styling to container ui and aboutpage

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,59 +8,51 @@ const AboutPage = () => {
 
       <div className={styles.itemContainer}>
         <h2 className={styles.itemHeader}>What</h2>
-        <Container>
-          <div className={styles.textContainer}>
-            <p>
-              The Daily Derivative is a daily-calculus-problem-with-solution
-              game.
-            </p>
-            <p>
-              Behind the scenes, users are entering latex code through the
-              on-screen keyboard. The user&apos;s input is stored into a string,
-              then compared to the answers that have been assigned to the given
-              problem.
-            </p>
-            <p>
-              Users can sign-in and keep track of stats that include: current
-              winning steak, the amount of problems attempted and the amount of
-              problems that have been answered correctly.
-            </p>
-          </div>
-        </Container>
+        <div className={styles.textContainer}>
+          <p>
+            The Daily Derivative is a daily-calculus-problem-with-solution game.
+          </p>
+          <p>
+            Behind the scenes, users are entering latex code through the
+            on-screen keyboard. The user&apos;s input is stored into a string,
+            then compared to the answers that have been assigned to the given
+            problem.
+          </p>
+          <p>
+            Users can sign-in and keep track of stats that include: current
+            winning steak, the amount of problems attempted and the amount of
+            problems that have been answered correctly.
+          </p>
+        </div>
       </div>
       <div className={styles.itemContainer}>
         <h2 className={styles.itemHeader}>When</h2>
-        <Container>
-          <div className={styles.textContainer}>
-            <p>
-              The Daily Derivative was created in 2022 by computer science
-              student (at time of writing), Chris Ragland.
-            </p>
-            <p>
-              Chris enjoys solving simple derivatives and integrals. He
-              describes it as, &lsquo;stretching for the brain.&rsquo;
-            </p>
-          </div>
-        </Container>
+        <div className={styles.textContainer}>
+          <p>
+            The Daily Derivative was created in 2022 by computer science student
+            (at time of writing), Chris Ragland.
+          </p>
+          <p>
+            Chris enjoys solving simple derivatives and integrals. He describes
+            it as, &lsquo;stretching for the brain.&rsquo;
+          </p>
+        </div>
       </div>
       <div className={styles.itemContainer}>
         <h2 className={styles.itemHeader}>Why</h2>
-        <Container>
-          <div className={styles.textContainer}>
-            <p>
-              The idea behind The Daily Derivative is to keep calculus skills
-              fresh.
-            </p>
-            <p>
-              The term &lsquo;Use it or loose it&rsquo; exists for a reason.
-            </p>
-            <p>
-              If a student is taking a semester or two between calculus classes,
-              TDD is perfect to keep concepts fresh and skills sharp.
-            </p>
-            <p>Or maybe you just enjoy calculus with your coffee ☕️</p>
-          </div>
-        </Container>
+        <div className={styles.textContainer}>
+          <p>
+            The idea behind The Daily Derivative is to keep calculus skills
+            fresh.
+          </p>
+          <p>The term &lsquo;Use it or loose it&rsquo; exists for a reason.</p>
+          <p>
+            If a student is taking a semester or two between calculus classes,
+            The Daily Derivative is perfect to keep concepts fresh and skills
+            sharp.
+          </p>
+          <p>Or maybe you just enjoy calculus with your coffee ☕️</p>
+        </div>
       </div>
     </div>
   );
