e9b9b406330ac1968f6942c47623a1e58ff3ecd9
Chris Ragland <Chrisragland97@gmail.com>
2022-06-08 17:59:02 -0400

    numbers on stats page are alot better styling and functionality

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,7 +1,8 @@
 import styles from "./StatsCard.module.css";
 import Container from "./ui/Container";
 //@ts-ignore
-import AnimatedNumber from "animated-number-react";
+import { animate } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 //stats props interface
 interface StatsCardProps {
@@ -10,22 +11,36 @@ interface StatsCardProps {
 }
 
 const StatsCard = (props: StatsCardProps) => {
-  //set the format of the animated number
-  const formatValue = (value: number) => `${Number(value).toFixed(0)}`;
   return (
     <Container>
       <h2 className={styles.cardHeader}>{props.title}</h2>
       <div className={styles.numberContainer}>
-        <AnimatedNumber
-          delay={618}
-          formatValue={formatValue}
-          className={styles.number}
-          value={props.numberToAnimate}
-          duration={1000}
-        />
+        <AnimateNumber from={0} to={props.numberToAnimate} />
       </div>
     </Container>
   );
 };
+
+//animated number using framer motion
+function AnimateNumber({ from, to }: any) {
+  //using useRef so it does not rerender each time a number changes
+  const nodeRef = useRef<any>();
+
+  useEffect(() => {
+    const node = nodeRef.current;
+
+    const controls = animate(from, to, {
+      duration: 3,
+      onUpdate(value) {
+        node.textContent = value.toFixed(0);
+      },
+      ease: "easeOut",
+    });
+
+    return () => controls.stop();
+  }, [from, to]);
+
+  return <p className={styles.number} ref={nodeRef} />;
+}
 
 export default StatsCard;
