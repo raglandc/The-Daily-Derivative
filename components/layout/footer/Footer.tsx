cad49a75f683f9b67f68b6e4e8d50eb7a194d9b5
Chris Ragland <Chrisragland97@gmail.com>
2022-06-19 18:08:24 -0400

    add message to footer of layout

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,9 +1,23 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
+  //create dynamic year the developer does not need to update the copy right year every year
+  const currentTime = new Date(Date.now());
+  const currentYear = currentTime.getFullYear();
+
   return (
     <div className={styles.container}>
-      <div>&copy; 2022</div>
+      <div>&copy; {currentYear}</div>
+      <p>
+        made with ❤️ by{" "}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://vidaldevelopers.com"
+        >
+          Vidal Developers
+        </a>
+      </p>
     </div>
   );
 };
